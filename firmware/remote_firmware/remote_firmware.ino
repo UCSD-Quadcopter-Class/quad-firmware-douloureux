e39677ba94df5f3b1c6dd3a75ea6e718e0dc7a51
Zhe Wang <zhw199@ucsd.edu>
2017-04-24 19:50:35 -0700

    samll change on quad-romote

--- firmware/remote_firmware/remote_firmware.ino
+++ firmware/remote_firmware/remote_firmware.ino
@@ -51,35 +51,26 @@ void loop()
   roll_val = analogRead(roll);    
   pitch_val = analogRead(pitch);  
 
-  if(yaw_val > yaw_max) { yaw_max = yaw_val; }
+  if(yaw_val > yaw_max) { yaw_max = yaw_val; yaw_lcd = ((yaw_val - yaw_min) *32) / ((yaw_max - yaw_min)/8);}
   if(throttle_val > throttle_max) { throttle_max = throttle_val; }
   if(roll_val > roll_max) { roll_max = roll_val; }
   if(pitch_val > pitch_max) { pitch_max = pitch_val; }
   
-  if(yaw_val < yaw_min) { yaw_min = yaw_val; }
+  if(yaw_val < yaw_min) { yaw_min = yaw_val; yaw_lcd = ((yaw_val - yaw_min) *32) / ((yaw_max - yaw_min)/8);}
   if(throttle_val < throttle_min) { throttle_min = throttle_val; }
   if(roll_val < roll_min) { roll_min = roll_val; }
   if(pitch_val < pitch_min) { pitch_min = pitch_val; }
 
-  yaw_d = yaw_max - yaw_min;
-  throttle_d = throttle_max - throttle_min;
-  roll_d = roll_max - roll_min;
-  pitch_d = pitch_max - pitch_min;
   
-  yaw_lcd = ((yaw_val - yaw_min) *25) / (yaw_d/10);
-  throttle_lcd = (throttle_val- throttle_min)*25/(throttle_d/10);
-  roll_lcd = (roll_val - roll_min)*25/(roll_d/10);
-  pitch_lcd = (pitch_val - pitch_min) *25/(pitch_d/10);
+  throttle_lcd = (throttle_val- throttle_min)*32/((throttle_max - throttle_min)/8);
+  roll_lcd = (roll_val - roll_min)*32/((roll_max - roll_min)/8);
+  pitch_lcd = (pitch_val - pitch_min) *32/((pitch_max - pitch_min)/8);
 
   
-  
+  rfWrite(throttle_lcd);
   lcd.clear();
   lcd.selectLine(0);
-
-  
-  //lcd.print(digitalRead(13));
   lcd.print("y: "); lcd.print(yaw_lcd);
-  //lcd.print("  t: "); lcd.print(throttle_val);
   lcd.print("  t: "); lcd.print(throttle_lcd);
   lcd.selectLine(1);
   lcd.print("r: "); lcd.print(roll_lcd);
