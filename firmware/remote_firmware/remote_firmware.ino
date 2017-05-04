267aee532b126b71f6c49a4a34800b2993f30b56
Jwong104 <Jwong104@gmail.com>
2017-05-03 20:47:41 -0700

    Remote firmware

--- firmware/remote_firmware/remote_firmware.ino
+++ firmware/remote_firmware/remote_firmware.ino
@@ -1,36 +1,60 @@
 #include <serLCD.h>
 #include "radio.h"
-int yaw = 0;
-int throttle = 1;
-int roll = 2;
-int pitch = 3;
+double yaw = 0;
+double throttle = 1;
+double roll = 2;
+double pitch = 3;
 
 double yaw_lcd = 0;
 double throttle_lcd = 0;
 double roll_lcd = 0;
 double pitch_lcd = 0;
 
-int yaw_val = 0;
-int throttle_val = 0;
-int roll_val = 0;
-int pitch_val = 0;
+double yaw_val = 0;
+double throttle_val = 0;
+double roll_val = 0;
+double pitch_val = 0;
 
-int yaw_max = 0;
-int throttle_max = 0; 
-int roll_max = 0;
-int pitch_max = 0; 
+double yaw_max = 0;
+double throttle_max = 0; 
+double roll_max = 0;
+double pitch_max = 0; 
 
-int yaw_min = 500;
-int throttle_min = 500;
-int roll_min = 500;
-int pitch_min = 500;
+double yaw_min = 500;
+double throttle_min = 500;
+double roll_min = 500;
+double pitch_min = 500;
 
-int yaw_d = 0;
-int throttle_d = 0;
-int roll_d = 0;
-int pitch_d = 0;
+double yaw_d = 0;
+double throttle_d = 0;
+double roll_d = 0;
+double pitch_d = 0;
 
 serLCD lcd;
+
+typedef struct{
+  int y;
+  int t;
+  int r;
+  int p;
+
+} Copter;
+
+Copter rf_copter;
+
+void rf_sent() {
+
+  
+  rfWrite( (uint8_t*)(&rf_copter) , (uint8_t)sizeof(rf_copter));
+
+  
+}
+
+Copter * cptr;
+
+void rf_receive() {
+  rfRead( (uint8_t*)cptr , (uint8_t)sizeof(rf_copter));
+}
 
 void setup()
 
@@ -51,32 +75,38 @@ void loop()
   roll_val = analogRead(roll);    
   pitch_val = analogRead(pitch);  
 
-  if(yaw_val > yaw_max) { yaw_max = yaw_val; yaw_lcd = ((yaw_val - yaw_min) *32) / ((yaw_max - yaw_min)/8);}
+  if(yaw_val > yaw_max) { yaw_max = yaw_val;}
   if(throttle_val > throttle_max) { throttle_max = throttle_val; }
   if(roll_val > roll_max) { roll_max = roll_val; }
   if(pitch_val > pitch_max) { pitch_max = pitch_val; }
   
-  if(yaw_val < yaw_min) { yaw_min = yaw_val; yaw_lcd = ((yaw_val - yaw_min) *32) / ((yaw_max - yaw_min)/8);}
+  if(yaw_val < yaw_min) { yaw_min = yaw_val;}
   if(throttle_val < throttle_min) { throttle_min = throttle_val; }
   if(roll_val < roll_min) { roll_min = roll_val; }
   if(pitch_val < pitch_min) { pitch_min = pitch_val; }
 
   
-  throttle_lcd = (throttle_val- throttle_min)*32/((throttle_max - throttle_min)/8);
-  roll_lcd = (roll_val - roll_min)*32/((roll_max - roll_min)/8);
-  pitch_lcd = (pitch_val - pitch_min) *32/((pitch_max - pitch_min)/8);
+  throttle_lcd = (throttle_val- throttle_min)*140/(throttle_max - throttle_min);
+  roll_lcd = (roll_val - roll_min)*140/(roll_max - roll_min);
+  pitch_lcd = (pitch_val - pitch_min) *140/(pitch_max - pitch_min);
+  yaw_lcd = (yaw_val - yaw_min) * 140 / (yaw_max - yaw_min);
 
+
+  
   
   rfWrite(throttle_lcd);
   lcd.clear();
   lcd.selectLine(0);
-  lcd.print("y: "); lcd.print(yaw_lcd);
-  lcd.print("  t: "); lcd.print(throttle_lcd);
+  lcd.print("y: "); lcd.print((signed)yaw_lcd);
+  lcd.print("  t: "); lcd.print((signed)throttle_lcd);
   lcd.selectLine(1);
-  lcd.print("r: "); lcd.print(roll_lcd);
-  lcd.print("  p: "); lcd.print(pitch_lcd);
+  lcd.print("r: "); lcd.print((signed)roll_lcd);
+  lcd.print("  p: "); lcd.print((signed)pitch_lcd);
   
-  
-  
+  rf_copter.t = throttle_lcd;
+  rf_copter.y = yaw_lcd;
+  rf_copter.r = roll_lcd;
+  rf_copter.p = pitch_lcd;
+  rf_sent();
 
 }
