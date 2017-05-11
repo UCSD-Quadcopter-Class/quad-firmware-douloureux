b2aa80dea4ed5a7e9b76c7c1bd78418b977194da
Jwong104 <Jwong104@gmail.com>
2017-05-11 10:42:53 -0700

    Firmware

--- firmware/remote_firmware/remote_firmware.ino
+++ firmware/remote_firmware/remote_firmware.ino
@@ -1,10 +1,9 @@
 #include <serLCD.h>
 #include "radio.h"
-double yaw = 0;
-double throttle = 1;
-double roll = 2;
-double pitch = 3;
+#include "quad_firmware.h"
 
+double btn1 = 0;
+double btn2 = 0;
 double yaw_lcd = 0;
 double throttle_lcd = 0;
 double roll_lcd = 0;
@@ -30,6 +29,13 @@ double throttle_d = 0;
 double roll_d = 0;
 double pitch_d = 0;
 
+double p1 = 0;
+double p2 = 0;
+
+double pen1 = 0;
+double pen2 = 0;
+
+
 serLCD lcd;
 
 typedef struct{
@@ -37,6 +43,10 @@ typedef struct{
   int t;
   int r;
   int p;
+  int b1;
+  int b2;
+  int pen1;
+  int pen2;
 
 } Copter;
 
@@ -60,20 +70,42 @@ void setup()
 
 {
   rfBegin(13);
-  Serial.begin(9600);          //  setup serial
+  Serial.begin(115200);          //  setup serial
   lcd.clear();
   lcd.display();
   lcd.setBrightness(20);
+  rf_copter.b1 = 1;
+  rf_copter.b2 = 1;
+  
 }
 
 void loop()
 
 {
   delay(1500);
-  yaw_val = analogRead(yaw); 
-  throttle_val = analogRead(throttle);    
-  roll_val = analogRead(roll);    
-  pitch_val = analogRead(pitch);  
+  pinMode(PIN_BTN1, INPUT_PULLUP);            // Button 1
+  pinMode(PIN_BTN2, INPUT_PULLUP);            // Button 2
+
+  btn1 = digitalRead(PIN_BTN1); 
+  btn2 = digitalRead(PIN_BTN2);
+
+  rf_copter.b1 = btn1;
+  rf_copter.b1 = btn2;
+   
+  
+  pen1 = analogRead(PIN_POT1); 
+  pen2 = analogRead(PIN_POT2);
+
+  p1 = (((pen1 - POT_MIN) * (POT_MAXEW)) / (POT_MAX - POT_MIN)) + POT_MINEW;
+  p2 = (((pen2 - POT_MIN) * (POT_MAXEW)) / (POT_MAX - POT_MIN)) + POT_MINEW;
+
+  rf_copter.pen1 = p1;
+  rf_copter.pen2 = p2;
+  
+  yaw_val = analogRead(PIN_YAW); 
+  throttle_val = analogRead(PIN_THROTTLE);    
+  roll_val = analogRead(PIN_ROLL);    
+  pitch_val = analogRead(PIN_PITCH);  
 
   if(yaw_val > yaw_max) { yaw_max = yaw_val;}
   if(throttle_val > throttle_max) { throttle_max = throttle_val; }
@@ -85,15 +117,11 @@ void loop()
   if(roll_val < roll_min) { roll_min = roll_val; }
   if(pitch_val < pitch_min) { pitch_min = pitch_val; }
 
-  
   throttle_lcd = (throttle_val- throttle_min)*140/(throttle_max - throttle_min);
   roll_lcd = (roll_val - roll_min)*140/(roll_max - roll_min);
   pitch_lcd = (pitch_val - pitch_min) *140/(pitch_max - pitch_min);
   yaw_lcd = (yaw_val - yaw_min) * 140 / (yaw_max - yaw_min);
 
-
-  
-  
   rfWrite(throttle_lcd);
   lcd.clear();
   lcd.selectLine(0);
@@ -108,5 +136,5 @@ void loop()
   rf_copter.r = roll_lcd;
   rf_copter.p = pitch_lcd;
   rf_sent();
-
+  
 }
