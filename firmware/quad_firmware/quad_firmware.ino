7c1563da40293aa4da8514bdb864ebe4199d27a5
Zhe Wang <zhw199@ucsd.edu>
2017-05-18 10:24:03 -0700

    pid

--- firmware/quad_firmware/quad_firmware.ino
+++ firmware/quad_firmware/quad_firmware.ino
@@ -6,9 +6,30 @@
 #include <Adafruit_LSM9DS1.h>
 #include <Adafruit_Sensor.h>  // not used in this demo but required!
 
-// i2c
 Adafruit_LSM9DS1 lsm = Adafruit_LSM9DS1();
 Adafruit_Simple_AHRS ahrs(&lsm.getAccel(), &lsm.getMag());
+
+  int y1;
+  int t1;
+  int r1;
+  int p1;
+  int b11;
+  int b21;
+  double pen11;
+  double pen21;
+
+  int initialize = 0;
+
+  int yR;
+  int tR;
+  int rR;
+  int pR;
+  int b1R;
+  int b2R;
+  double pen1R;
+  double pen2R;
+
+
 
 
 int pb5 = 8;
@@ -19,15 +40,15 @@ int pe4 = 4;
 double range = 10;
 
 double FL = 150;
-double FR = 155;
-double BL = 115;
-double BR = 120;
+double FR = 150;
+double BL = 120;
+double BR = 130;
 
 double roll_input = 0;
 double pitch_input = 0;
 
-double roll_last_input = 0;
-double pitch_last_input = 0;
+double roll_last_error = 0;
+double pitch_last_error = 0;
 
 double roll_cur = 0;
 double pitch_cur = 0;
@@ -46,9 +67,14 @@ double kp = 0;
 double ki = 0;
 double kd = 0;
 
-double duration = 1;
+double duration = 0;
 
-double last_time = 0;
+unsigned int last_time = 0;
+int loop_count = 0;
+
+
+float pitch;
+float roll;
 
 typedef struct{
   int y;
@@ -82,11 +108,11 @@ void setupPID(double a, double b, double c, double min_value, double max_value){
 
   i_min = min_value;
   i_max = max_value;
-  last_time = millis();
+  
 }
 
 void pid(){
-  if(millis() - last_time < duration) return;
+  if(abs(millis() - last_time) < duration) return;
   last_time = millis();
   roll_error = roll_input - roll_cur;
   i_term += roll_error * ki;
@@ -94,30 +120,79 @@ void pid(){
   if(i_term < i_min ) i_term = i_min;
   
   
-  roll_output = kp * roll_error + i_term - (roll_input - roll_last_input) * kd; 
+  roll_output = kp * roll_error + i_term + (roll_error - roll_last_error) * kd; 
   
-  
-//  Serial.print("\tP: ");Serial.print(kp * roll_error);
+  Serial.print("\tP: ");Serial.print(kp * roll_error);
 //  Serial.print("\tI: ");Serial.print(i_term);
-//  Serial.print("\tD: ");Serial.print((roll_input - roll_last_input) * kd);
-//  Serial.print("\tOUTPUT: ");Serial.println(roll_output);
-  roll_last_input = roll_input;
+  Serial.print("\tD: ");Serial.print((roll_error - roll_last_error) * kd);
+  Serial.print("\tOUTPUT: ");Serial.println(roll_output);
+  roll_last_error = roll_error;
 }
 
 
+
+int rfError(){
+
+    yR = cptr.y;
+    tR = cptr.t;
+    rR = cptr.r;
+    pR = cptr.p;
+    pen1R = cptr.pen1;
+    pen2R = cptr.pen2;
+  if( initialize == 0 ) { 
+    initialize = 1; 
+    y1 = cptr.y;
+    t1 = cptr.t;
+    r1 = cptr.r;
+    p1 = cptr.p;
+    pen11 = cptr.pen1;
+    pen21 = cptr.pen2;
+   
+    return 1; 
+  }
+
+  if( (0 < abs(y1 - cptr.y) < 30)  ) { y1 = cptr.y; }
+  else if( 0 < (abs(cptr.y - yR) < 30) ) { y1 = cptr.y; cptr.y = yR; }
+  else { cptr.y = y1; }
+  
+  if( (0 <abs(t1 - cptr.t) < 30) ) { t1 = cptr.t;}
+  else if( (0 <abs(cptr.t - tR) < 30) ) { t1 = cptr.t; cptr.t = tR; }
+  else { cptr.t = t1;}
+  
+  if( (0 <abs(r1 - cptr.r) < 30) ) { r1 = cptr.r; }
+  else if( (abs(cptr.r - rR) < 30) ) { r1 = cptr.r; cptr.r = rR; }
+  else{ cptr.r = r1; }
+  
+  if( (0 <abs(p1 - cptr.p) < 30) ) { p1 = cptr.p; }
+  else if( (0 <abs(cptr.p - pR) < 30)) { p1 = cptr.p; cptr.p = pR; }
+  else{ cptr.p = p1; }
+  
+  if( (0 <abs(pen11 - cptr.pen1) < 80) ) { pen11 = cptr.pen1; }
+  else if( (0 <abs(cptr.pen1 - pen1R) < 80) ) { pen11 = cptr.pen1; cptr.pen1 = pen1R; }
+  else{ cptr.pen1 = pen11; }
+  
+  if( (0 <abs(pen21 - cptr.pen2 ) < 80) ) { pen21 = cptr.pen2; }
+  else if( (0 <abs(cptr.pen2 - pen2R) < 80) ) { pen21 = cptr.pen2; cptr.pen2 = pen2R; }
+  else{ cptr.pen2 = pen21; }
+    
+
+  return 1;
+  
+}
 
 void rf_receive() {
   rfRead( (uint8_t*)(&cptr) , (uint8_t)sizeof(Copter));
-  Serial.print("y: "); Serial.print(cptr.y);
-  Serial.print("t: "); Serial.print(cptr.t);
-  Serial.print("r: "); Serial.print(cptr.r);
-  Serial.print("p: "); Serial.print(cptr.p);
-  Serial.print("b1: "); Serial.print(cptr.b1);
-  Serial.print("b2: "); Serial.print(cptr.b2);
-  Serial.print("pen1: "); Serial.print(cptr.pen1);
-  Serial.print("pen2: "); Serial.println(cptr.pen2);
+  if(  rfError() ) {
+//    Serial.print("y: "); Serial.print(cptr.y);
+//    Serial.print("t: "); Serial.print(cptr.t);
+//    Serial.print("r: "); Serial.print(cptr.r);
+//    Serial.print("p: "); Serial.print(cptr.p);
+//    Serial.print("b1: "); Serial.print(cptr.b1);
+//    Serial.print("b2: "); Serial.print(cptr.b2);
+//    Serial.print("pen1: "); Serial.print(cptr.pen1);
+//    Serial.print("pen2: "); Serial.println(cptr.pen2);
+  }
 }
-
 
 
 void setup() {
@@ -134,56 +209,49 @@ void setup() {
     while (1);
   }
   setupSensor();
-  setupPID(1.3,0,0,-100,100);
+  last_time = millis();
+  setupPID(cptr.pen2,0,20,-100,100);
 }
 
 void loop(){
   sensors_vec_t orientation;
   ahrs.getOrientation(&orientation);
+
+
+  sensors_event_t a, m, g, temp;
+  lsm.getEvent(&a, &m, &g, &temp);
+  setupPID(cptr.pen2,0,cptr.pen1,-100,100);
+//  setupPID(1.3,0,20,-100,100);
+ 
+
+  float roll_g = g.gyro.x;
+  float pitch_g = g.gyro.y;
+
+  roll = 0.9*(roll + g.gyro.x * 0.001) + 0.1 * orientation.roll;
+  pitch = 0.9*(pitch + g.gyro.y * 0.001) + 0.1 * orientation.pitch;
+
   
-//  Serial.print("\tOrientation roll: "); Serial.print(orientation.roll);
-//  Serial.print("\tOrientation pitch: "); Serial.println(orientation.pitch);
+//  Serial.print("\troll: "); Serial.print(roll);
+//  Serial.print("\tpitch: "); Serial.println(pitch);
   roll_input = 0;
-  roll_cur = orientation.pitch;
-  //pid();
-
-
-  
-//  if(FL - roll_output > range)  FL = range;
-//  else if(FL-roll_output < -range) FL = -range;
-//  else  FL = FL-roll_output;
-//
-//  if(FR - roll_output > range)  FR = range;
-//  else if(FR-roll_output < -range) FR = -range;
-//  else  FR = FR-roll_output;
-//
-//  if(BR + roll_output > range)  BR = range;
-//  else if(BR+roll_output < -range) BR = -range;
-//  else  BR = BR+roll_output;
-//
-//  if(BL + roll_output > range)  BL = range;
-//  else if(BL+roll_output < -range) BL = -range;
-//  else  BL = BL+roll_output;
-//
-//
-
-
-//  analogWrite(pe5, 100); //FR
-//  analogWrite(pb5, 100); // FL
-//  analogWrite(pe3, 100); //BL
-//  analogWrite(pe4, 100); //BR 
+  roll_cur = pitch;
+  pid();
 
   //Serial.print("\tFR: ");Serial.print(FR);
   //Serial.print("\tFL: ");Serial.print(FL);
-//  Serial.print("\tBL: ");Serial.print(BL);
-//  Serial.print("\tBR: ");Serial.println(BR);
+  //Serial.print("\tBL: ");Serial.print(BL);
+  //Serial.print("\tBR: ");Serial.println(BR);
+//  //Serial.print("\tpen2: "); Serial.println(cptr.pen1);
+//  Serial.print("\troll output: "); Serial.println(roll_output);
+  //Serial.print("\troll: "); Serial.println(pitch);
 
-//  analogWrite(pe5, FR-roll_output); //FR
-//  analogWrite(pb5, FL-roll_output); // FL
-//  analogWrite(pe3, BL+roll_output); //BL
-//  analogWrite(pe4, BR+roll_output); //BR 
+  
+  analogWrite(pe5, 120 + cptr.t-roll_output); //FR
+  analogWrite(pb5, 120 + cptr.t-roll_output); // FL
+  analogWrite(pe3, 120+roll_output); //BL
+  analogWrite(pe4, 120+roll_output); //BR 
 
-
+//
   if(rfAvailable())
     rf_receive();
 }
